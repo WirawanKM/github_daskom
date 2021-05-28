89458648d9e07a0d05e30b9956c578b4fd3b30ac
Wirawan <wirawankhairulmajid@gmail.com>
2021-05-28 15:24:31 +0700

    default

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Praktikum-PTW</title>
+</head>
+<body>
+    <h1>Tugas Kegiatan Praktikum 1 - Wirawan Khairul Majid</h1>
+    <hr/>
+    <a href={{url('/artikel')}} >artikel</a> |
+
+</body>
+</html>
